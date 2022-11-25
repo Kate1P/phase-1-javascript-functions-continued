31e5a78f91eba22f2f38c3d646e19e6533d95737
Kate Popova <popova1ea@gmail.com>
2022-11-25 11:55:54 -0800

    commit tree 9cb85f0a9d466563cea7aa5e1d42fdc3b79fe7a9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(x) {
+if (typeof x === "undefined") {
+        x = "roller-skate"}
+return `This Saturday, I want to ${x}!`
+}
+let mondayWork = function(x) {
+    if (typeof x === "undefined") {
+            x = "go to the office"}
+    return `This Monday, I will ${x}.`
+    }
+    function wrapAdjective (style="*") {
+        return function(stringOne="special"){
+          return `You are ${style}${stringOne}${style}!`
+        }
+      }
